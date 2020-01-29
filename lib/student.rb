405d03d2470ce0f4096836139a4e26c4b1d31b03
Daniel Reina <dannyflatiron@gmail.com>
2020-01-29 03:59:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
       end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(grade_of_class)
     sql = <<-SQL
       SELECT *
       FROM students
