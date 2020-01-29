eebb7ee5d533a7b7b2b605612189cb8c854a2b3e
Daniel Reina <dannyflatiron@gmail.com>
2020-01-29 03:52:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,19 @@ class Student
       end.first
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, grade).map do |row|
+        self.new_from_db(row)
+      end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
